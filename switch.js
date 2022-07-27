e26a5d5d9377f22f35bb72135621b4016b4a785a
fabuloso01 <fabuloso01@github.com>
2022-07-27 15:37:24 -0400

    commit

---
+++ switch.js
@@ -0,0 +1,28 @@
+let metodo = "Dinheiro"
+
+
+switch(metodo) {
+    case "DInheiro":
+        alert("Pagamento em dinheiro")
+        break;
+    case "Cartão de débito":
+        alert("Pagamento em cartão de débito")
+        break;
+    case "Cartão de crédito":
+        alert("Pagamento em cartão de crédito")
+        break;
+    case "Cheque":
+        alert("Pagamento em cheque")
+        break;
+    case "Carnê":
+        alert("Pagamento por carnê")
+        break;
+    case "Vale alimentação":
+        alert("Pagamento por vale alimentação")
+        break;
+    default:
+        alert("Opção de pagamento inválida!")
+        break;
+     
+        
+}
